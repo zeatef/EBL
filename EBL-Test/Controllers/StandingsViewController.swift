6cc3eb50a03fe799c73c9c211c5a6fe6689019ab
zeatef <zeatef@gmail.com>
2019-05-29 01:49:04 +0200

    #7: Add Standings View Controller

---
+++ EBL-Test/Controllers/StandingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StandingsViewController.swift
+//  EBL-Test
+//
+//  Created by Zeyad Atef on 5/29/19.
+//  Copyright © 2019 Zeyad Atef. All rights reserved.
+//
+
+import UIKit
+
+class StandingsViewController: UIViewController {
+
+    
+    @IBOutlet weak var groupsView: UIScrollView!
+    @IBOutlet weak var tableView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
